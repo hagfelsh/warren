66fa7c1b58e7a2c78911eab2c9a696519ebff6c5
Chris Keiser <chris_keiser@dell.com>
2022-04-13 13:53:58 -0500

    Initial commit

---
+++ warren.py
@@ -0,0 +1,14 @@
+# PSEUDO CODE
+# introducion
+# Questions about the character - they adjust an alignment score between lwaful good - chaotic evil
+# ask for the character name
+#
+# print the alignment
+
+# additional features
+# add stats with additional questions
+# devise a player class based on alignment and more questions
+# pretty-print a character sheet (in ascii of course because we're cavemen)
+# offer to save the character sheet in json, csv, plaintext
+# save it to a database
+# create an editor to adjust the stats and get a different result
